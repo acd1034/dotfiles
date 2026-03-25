6dcf16e3fbec47f6a1eba963a3bbe6a864b0f08c
acd1034 <48613285+acd1034@users.noreply.github.com>
2026-03-25 09:05:30 +0900

    Update repoinit.mk

--- vscode/repoinit.mk
+++ vscode/repoinit.mk
@@ -1,13 +1,18 @@
+define log_success
+	@printf "\033[0;32m[SUCCESS]\033[0m %s\n" "$(1)"
+endef
+
 SRC := $(HOME)/.dotfiles/vscode
 DEST := .
 
 # -R: 再帰的, -v: 詳細表示, -i: 上書き前に確認
 .PHONY: python
 python: ## Python開発環境を初期化
-	@cp -vi $(SRC)/python/gitignore $(DEST)/.gitignore
-	@mkdir -p $(DEST)/.vscode
-	@cp -vi $(SRC)/python/*.json $(DEST)/.vscode/
-	@cp -vi $(SRC)/python/vscode_gitignore $(DEST)/.vscode/.gitignore
+	cp -i $(SRC)/python/gitignore $(DEST)/.gitignore
+	mkdir -p $(DEST)/.vscode
+	cp -i $(SRC)/python/*.json $(DEST)/.vscode/
+	cp -i $(SRC)/python/vscode_gitignore $(DEST)/.vscode/.gitignore
+	$(call log_success,Done!)
 
 .DEFAULT_GOAL := help
 .PHONY: help
