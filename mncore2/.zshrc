5613ae6af2e9601fafce53dab0d989f978ec951b
acd1034 <48613285+acd1034@users.noreply.github.com>
2026-04-06 18:06:24 +0900

    feat(mncore2): add Prezto configuration files for mncore2 env

---
+++ mncore2/.zshrc
@@ -0,0 +1,40 @@
+#
+# Executes commands at the start of an interactive session.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+# Customize to your needs...
+log_error() {
+  printf "\033[0;31m[ERROR]\033[0m %s\n" "$1"
+}
+log_success() {
+  printf "\033[0;32m[SUCCESS]\033[0m %s\n" "$1"
+}
+log_warning() {
+  printf "\033[0;33m[WARNING]\033[0m %s\n" "$1"
+}
+log_info() {
+  printf "\033[0;36m[INFO]\033[0m %s\n" "$1"
+}
+
+# aliases
+alias amend='git commit --amend --no-edit'
+alias beep='tput bel'
+alias jpeg='mkdir -p jpeg && sips -s format jpeg *.* --out jpeg/'
+alias echopath='echo $PATH | tr ":" "\n"'
+alias gedit='code ~/.config/ghostty/config'
+alias repoinit='make -f ~/.dotfiles/vscode/repoinit.mk'
+# TODO
+# alias ls='eza'
+# alias grep='rg'
+# eval "$(zoxide init zsh --cmd cd)" # cd='zoxide'
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh # keybindings for fzf
---
+++ mncore2/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+log_error() {
+  printf "\033[0;31m[ERROR]\033[0m %s\n" "$1"
+}
+log_success() {
+  printf "\033[0;32m[SUCCESS]\033[0m %s\n" "$1"
+}
+log_warning() {
+  printf "\033[0;33m[WARNING]\033[0m %s\n" "$1"
+}
+log_info() {
+  printf "\033[0;36m[INFO]\033[0m %s\n" "$1"
+}
+
+# 失敗したらスクリプトを終了
+set -euo pipefail
+
+# ホームディレクトリに移動
+cd "$HOME"
+log_info "Pwd: $(pwd)"
+
+# prezto
+log_info "Installing prezto..."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git config --global --add safe.directory "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(zpreztorc|zshrc|README.md)(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+ln -sfnv "${ZDOTDIR:-$HOME}"/.dotfiles/mncore2/.zpreztorc "${ZDOTDIR:-$HOME}"/.zpreztorc
+ln -sfnv "${ZDOTDIR:-$HOME}"/.dotfiles/mncore2/.zshrc "${ZDOTDIR:-$HOME}"/.zshrc
+
+# TODO: fzf
+# log_info "Installing fzf..."
+# $(brew --prefix)/opt/fzf/install --all
+
+log_success "Finished!"
+tput bel
