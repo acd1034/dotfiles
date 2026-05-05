60c130536c2530952289398557514d5bdc70b1a9
acd1034 <48613285+acd1034@users.noreply.github.com>
2026-05-05 10:58:04 +0900

    Refactor repoinit.mk

--- vscode/repoinit.mk
+++ vscode/repoinit.mk
@@ -2,25 +2,25 @@ define log_success
 	@printf "\033[0;32m[SUCCESS]\033[0m %s\n" "$(1)"
 endef
 
-SRC := $(HOME)/.dotfiles/vscode
 DEST := .
 
-# -R: 再帰的, -v: 詳細表示, -i: 上書き前に確認
 .PHONY: python
+python: SRC := $(HOME)/.dotfiles/vscode/python
 python: ## Python開発環境を初期化
-	-cp -i $(SRC)/python/gitignore $(DEST)/.gitignore
+	-cp -i $(SRC)/gitignore $(DEST)/.gitignore
 	mkdir -p $(DEST)/.vscode
-	-cp -i $(SRC)/python/*.json $(DEST)/.vscode/
-	-cp -i $(SRC)/python/vscode_gitignore $(DEST)/.vscode/.gitignore
+	-cp -i $(SRC)/*.json $(DEST)/.vscode/
+	-cp -i $(SRC)/vscode_gitignore $(DEST)/.vscode/.gitignore
 	$(call log_success,Done!)
 
+# -s: ハードリンクではなくシンボリックリンク, -i: 上書き前に確認, -n: 既存のリンクがあっても中身を追わずに強制的に置き換え
 .PHONY: mncore2
 mncore2: SRC := /mnt/pvc-home/rhirakida/pfcomp/.vscode
 mncore2: ## mncore2
 	mkdir -p $(DEST)/.vscode
 	-cp -i $(SRC)/*.json $(DEST)/.vscode/
 	-cp -i $(SRC)/.gitignore $(DEST)/.vscode/.gitignore
-	ln -sfnv $(SRC)/.env $(DEST)/.vscode/.env
+	-ln -sin $(SRC)/.env $(DEST)/.vscode/.env
 	$(call log_success,Done!)
 
 .DEFAULT_GOAL := help
